3bfb5d282581128361d371f25ef5cbf7414a4fe0
unknown <tantichandan@gmail.com>
2024-04-02 02:49:15 -0700

    new

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,11 +13,11 @@ const links = [
     { name: 'US', href: '/US' },
     { name: 'Deposit', href: '/Deposit' },
     { name: 'Free', href: '/Free' },
-    { name: 'All', href: '/allproduct' },
     { name: 'Blog', href: '/Blog' },
+    { name: 'All', href: '/allproduct' },
     { name: 'Contact', href: '/email' },
-
-
+    { name: 'Card', href: '/Card' },
+    
 ]
 
 export default function Navbar() {
@@ -36,16 +36,16 @@ export default function Navbar() {
                     <h1 className='text-2xl md:text-4xl font-bold'>Casino<span className='text-primary'>bonus</span></h1>
                 </Link>
 
+                
 
-
-                <nav className={`gap-12 lg:flex backdrop-blur-xl md:backdrop-blur-lg sm:px-12 px-4 items-center justify-between lg:pb-0 z-50 absolute lg:static lg:z-auto left-0 w-full lg:w-auto lg:pl-0 transition-all duration-500 ease-in top-[73px] ${navbar ? 'right-0' : 'left-[100%]'}`}  >
+                <nav className={`gap-12 lg:flex backdrop-blur-xl md:backdrop-blur-lg md:px-10 px-4  lg:items-center md:items-center justify-between lg:pb-0 z-50 absolute lg:static lg:z-auto left-0 w-full lg:w-auto lg:pl-0 transition-all duration-500 ease-in top-[73px] ${navbar ? 'right-0' : 'left-[-100%]'}`}  >
 
                     {links.map((link, idx) => (
                         <div key={idx}>
 
                             {pathname === link.href ? (
 
-                                <Link onClick={() => setNavbar(!navbar)} className='text-lg font-semibold text-primary' href={link.href}>
+                                <Link onClick={()=> setNavbar(!navbar)} className='text-lg font-semibold text-primary' href={link.href}>
 
                                     {link.name}
 
@@ -54,7 +54,7 @@ export default function Navbar() {
 
                             ) : (
 
-                                <Link href={link.href} onClick={() => setNavbar(false)} className='text-lg font-semibold  text-gray-600 transition duration-100 hover:text-primary'>
+                                <Link href={link.href} onClick={()=> setNavbar(false)} className='text-lg font-semibold  text-gray-600 transition duration-100 hover:text-primary'>
                                     {link.name}
 
                                 </Link>
@@ -66,12 +66,12 @@ export default function Navbar() {
                         </div>
                     ))}
 
-
+                    
 
                 </nav>
 
                 <div className='flex divide-x border-r sm:border-l'>
-
+                    
                     <Button variant={'outline'} className='flex flex-col gap-y-1.5 h-12 sm:h-20 sm:w-20 md:w-24 rounded-none'>
                         <MessageSquare />
                         <span className='hidden text-xs font-semibold text-gray-500'>
